cce37aa0fdf8889fbdd2181c11aec438fcf14e7f
Can Burak Simsek <34836715+canburaksimsek@users.noreply.github.com>
2026-04-16 19:47:39 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000
   }
 })
-build: {
-  chunkSizeWarningLimit: 1000
-}
